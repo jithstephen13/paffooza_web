c82c8a115d0ec7166ff779591ea1c5afab6b634a
jithstephen <jithstephen@ikm.gov.in>
2024-08-06 14:47:53 +0530

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="about">
+    <h1>Coming soon......</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    text-align: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main class="mainContainer">
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+.mainContainer{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
